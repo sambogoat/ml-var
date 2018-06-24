b4df55155576aa793ffdb01c59ad855c1cca26cc
Stephen Matthew <stephen.matthew@gmail.com>
2018-06-24 10:23:17 +0800

    Init VaR ML project

---
+++ data/ppls.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+def generate_and_save_data(train_size=100, var_size=261, file='var.npz'):
+
+    # The % of VaR errors
+    errors = 0.20
+    ok_error_split = int(train_size * errors)
+
+    # The VaR 'features' that represent an vector in error, i.e. first, middle and last
+    features_in_error = [0, int(var_size / 2), var_size-1]
+
+    ppls = np.random.uniform(low=1.0, high=100.0, size=(train_size, var_size))
+
+    # Create the error ppls, i.e. setting the features to 0
+    error_ppls = ppls[0:ok_error_split]
+    for i in features_in_error:
+        error_ppls[:, i] = 0
+
+    # Error labels
+    error_labels = np.transpose(np.array([np.zeros(len(error_ppls), dtype=np.int32),
+                                          np.ones(len(error_ppls), dtype=np.int32)]))
+
+    # Valid ppls and labels, i.e. 1.
+    good_ppls = ppls[ok_error_split:len(ppls)]
+    good_labels = np.transpose(np.array([np.ones(len(good_ppls), dtype=np.int32),
+                                         np.zeros(len(good_ppls), dtype=np.int32)]))
+
+    # Concatenate the two arrays and shuffle maintaining relataive ordering between ppls and labels.
+    all_x = np.concatenate((good_ppls, error_ppls))
+    all_y = np.concatenate((good_labels, error_labels))
+    perm = np.random.permutation(train_size)
+    all_x_shuffled = all_x[perm]
+    all_y_shuffled = all_y[perm]
+    assert np.array_equal(np.where(np.amin(all_x_shuffled, axis=1) == 0)[0], np.where(all_y_shuffled[:,1] == 1)[0])
+
+    np.savez(file, x=all_x_shuffled, y=all_y_shuffled)
+    #np.savetxt("./ppls.csv", np.append(all_x_shuffled, all_y_shuffled, axis=1), delimiter=",", newline="\n")
+
+
+def load_data(file='var.npz'):
+    with np.load(file) as f:
+        x, y = f['x'], f['y']
+        return x, y
+
+generate_and_save_data(train_size=100000, var_size=261)
+
+generate_and_save_data(train_size=10000, var_size=261, file="var_test.npz")
+
---
+++ var.py
@@ -0,0 +1,182 @@
+from data import ppls
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+######################################################################################################################
+#
+# Links:
+#   http://jrmeyer.github.io/machinelearning/2016/02/01/TensorFlow-Tutorial.html
+#
+#
+# Results:
+#   5% errors, var_size=5, batch_size=1000, epochs=3, learning_rate=0.1 - Testing Accuracy =  0.9995
+#   5% errors, var_size=261, batch_size=1000, epochs=3, learning_rate=0.1 - Testing Accuracy =  0.95
+#   5% errors, var_size=261, batch_size=500, epochs=2,3, learning_rate=0.1 - Testing Accuracy =  0.95
+#   15% errors, var_size=261, batch_size=500,1000, epochs=2, learning_rate=0.1 - Testing Accuracy =  0.85
+#   15% errors, var_size=261, batch_size=1000, epochs=2, learning_rate=0.1 - VaR [1.0-2.0] - Testing Accuracy =  0.9997
+#   20% errors, var_size=261, batch_size=1000, epochs=2, learning_rate=0.1 - Testing Accuracy =  0.80
+#
+# <<< COST IS ALWAYS THE % ERRORS WHEN VAR IS 261; WHEN 5 THEN COST APPROACHES 0.
+#
+######################################################################################################################
+
+#############
+# Parameters
+#############
+
+learning_rate = 0.1
+
+training_epochs = 2
+
+batch_size = 1000
+
+# TODO - Move this to data module
+def next_batch(x, y, size, num):
+    s = num * size
+    e = s + size
+    return x[s:e], y[s:e]
+
+###################
+# Step 1 - Set-up
+###################
+
+# Load all the data
+X_train, y_train = ppls.load_data()
+
+num_features = X_train.shape[1]
+
+num_labels = y_train.shape[1]
+
+# Graph inputs
+X = tf.placeholder(tf.float32, [None, num_features])
+y = tf.placeholder(tf.float32, [None, num_labels])
+
+# Model weights
+W = tf.Variable(tf.truncated_normal([num_features, num_labels], stddev=0.1), name="W")
+b = tf.Variable(tf.truncated_normal([num_labels], stddev=0.1), name="b")
+
+init = tf.global_variables_initializer()
+
+##############################################################
+# Step 2 - Define Model (i.e. activation function)
+##############################################################
+
+yhat = tf.nn.sigmoid(tf.matmul(X, W) + b, name="activation")
+
+#####################################################
+# Step 3 - Definition Evaluation (i.e. cost function)
+#####################################################
+
+cost = tf.nn.l2_loss(yhat-y, name="squared_error_cost")
+# cost = tf.reduce_mean(tf.square(yhat - y))
+
+
+####################
+# Step 4 - Optimiser
+####################
+
+optimiser = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+################
+# Visualisations
+################
+
+# TODO - FIX: The plot does not update
+
+plt.ion()
+# Create the main, super plot
+fig = plt.figure()
+# Create two subplots on their own axes and give titles
+ax1 = plt.subplot("211")
+ax1.set_title("TRAINING ACCURACY", fontsize=18)
+ax2 = plt.subplot("212")
+ax2.set_title("TRAINING COST", fontsize=18)
+plt.tight_layout()
+plt.pause(.3)
+
+step_values = []
+accuracy_values = []
+cost_values = []
+
+########################
+# Step 5 - Training loop
+########################
+
+sess = tf.Session()
+
+sess.run(init)
+
+correct_prediction = tf.equal(tf.argmax(yhat, axis=1), tf.argmax(y, axis=1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+yhat_summary = tf.summary.histogram("yhat", yhat)
+
+cost_summary = tf.summary.scalar("cost", cost)
+
+accuracy_summary = tf.summary.scalar("accuracy", accuracy)
+
+weight_summary = tf.summary.histogram("weights", W.eval(session=sess))
+
+bias_summary = tf.summary.histogram("biases", b.eval(session=sess))
+
+all_summary = tf.summary.merge_all()
+
+writer = tf.summary.FileWriter("./summary", sess.graph)
+
+print("Training Epochs: {}".format(training_epochs))
+
+for epoch in range(training_epochs):
+
+    num_batches = int(len(X_train) / batch_size)
+
+    print("Number of batches: {} ({})".format(num_batches, batch_size))
+
+    for i in range(num_batches):
+
+        step = epoch*num_batches+i
+
+        x_train_batch, y_train_batch = next_batch(X_train, y_train, batch_size, i)
+
+        # Project the cols
+        # x_train = np.column_stack((x_train[:, 0], x_train[:, int(var_size/2)], x_train[:, var_size-1]))
+
+        train_data = {X: X_train, y: y_train}
+
+        sess.run(optimiser, feed_dict=train_data)
+
+        if step % 10 == 0:
+
+            step_values.append(step)
+
+            curr_accuracy, curr_cost, curr_summary = sess.run([accuracy, cost, all_summary], feed_dict=train_data)
+
+            accuracy_values.append(curr_accuracy)
+
+            cost_values.append(curr_cost)
+
+            writer.add_summary(curr_summary, step)
+
+            print(step, "accuracy=", curr_accuracy, ", cost=", curr_cost)
+
+            # Plot progress
+            ax1.plot(step_values, accuracy_values)
+            ax2.plot(step_values, cost_values)
+            fig.canvas.draw()
+
+
+####################
+# Step 6: Evaluation
+####################
+
+x_test, y_test = ppls.load_data(file='var_test.npz')
+
+# Project the cols
+# x_test = np.column_stack((x_test[:, 0], x_test[:, int(var_size / 2)], x_test[:, var_size - 1]))
+
+test_data = {X: x_test, y: y_test}
+print("Testing Accuracy = ", sess.run(accuracy, feed_dict = test_data))
+
+
